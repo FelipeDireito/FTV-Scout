d6360ae60db2c8abdc7116b4318a8017405a59a1
FelipeDireito <fedireito92@gmail.com>
2025-09-02 12:09:56 -0300

    chore: ativa a integridade com as FK das tabelas do banco

--- backend/src/core/database.py
+++ backend/src/core/database.py
@@ -1,11 +1,20 @@
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.engine import Engine
 from src.core.config import settings
 
 engine = create_engine(
     settings.DATABASE_URL, 
     connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}
 )
+
+# Ativa Foreign Key no SQLite
+if "sqlite" in settings.DATABASE_URL:
+    @event.listens_for(Engine, "connect")
+    def set_sqlite_pragma(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
