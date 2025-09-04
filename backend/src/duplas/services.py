586991e1c362bc522e9a20377f64502aa453fa0e
FelipeDireito <fedireito92@gmail.com>
2025-09-04 12:48:36 -0300

    style: espaçamento de 2 linhas entre funções

--- backend/src/duplas/services.py
+++ backend/src/duplas/services.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from src.duplas.models import Dupla
 from src.atletas.models import Atleta
 
+
 def criar_dupla(db: Session, nome_dupla: str, atletas_ids: list[int]):
     atletas = db.query(Atleta).filter(Atleta.atleta_id.in_(atletas_ids)).all()
     db_dupla = Dupla(nome_dupla=nome_dupla, atletas=atletas)
@@ -9,6 +10,7 @@ def criar_dupla(db: Session, nome_dupla: str, atletas_ids: list[int]):
     db.commit()
     db.refresh(db_dupla)
     return db_dupla
+
 
 def listar_dupla(db: Session):
     return db.query(Dupla).all()
