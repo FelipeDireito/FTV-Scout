b5800d13a72f13dcb56cb0ebdcc104f1802f70f6
FelipeDireito <fedireito92@gmail.com>
2025-09-17 23:33:36 -0300

    feat: caminhos para nova pagina de selecionar duplas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import NovaPartida from './pages/NovaPartida'
+import SelecionarDuplas from './pages/SelecionarDuplas';
+import Partida from './pages/Partida';
 // Crie e importe a página de Notação quando estiver pronta
 // import Notacao from './pages/Notacao';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* 
-        Com a remoção do Header, cada página agora é responsável
-        pelo seu próprio layout de ponta a ponta.
-      */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="dashboard" element={<Dashboard />} />
-        {/* 
-          As rotas para as páginas de "Nova Partida" e "Análise" podem ser adicionadas aqui.
-          Ex: <Route path="/new-match" element={<NewMatch />} />
-        */}
         <Route path="/nova-partida" element={<NovaPartida />} />
+        <Route path="/selecionar-duplas" element={<SelecionarDuplas />} />
+        <Route path="/partida" element={<Partida />} />
       </Routes>
     </BrowserRouter>
   );
