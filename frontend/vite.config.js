51e6486aeef8c4dc6e803f7799f1dcca376658d6
FelipeDireito <fedireito92@gmail.com>
2025-10-01 17:12:02 -0300

    feat: certificado para https, necessário ao PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      }),
+      basicSsl()
+    ],
+    preview: {
+      https: true,
+    },
+    server: {
+      https: true,
+      proxy: {
+        '/api': {
+          target: env.API_TARGET || 'http://localhost:8000',
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 })
