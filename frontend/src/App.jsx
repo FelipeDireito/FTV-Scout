e8b4d81df117eb6133609ad7c09ca8c463176eff
FelipeDireito <fedireito92@gmail.com>
2025-10-18 16:35:26 -0300

    feat: rotas para análises e estatísticas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
+import AnalisePartida from './pages/AnalisePartida';
 import NovaPartida from './pages/NovaPartida'
 import SelecionarDuplas from './pages/SelecionarDuplas';
 import Partida from './pages/Partida';
+import EstatisticasGerais from './pages/EstatisticasGerais';
 import PWABadge from '../PWABadge';
 
 function App() {
@@ -12,10 +14,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/analise/:partidaId" element={<AnalisePartida />} />
         <Route path="/nova-partida" element={<NovaPartida />} />
         <Route path="/selecionar-duplas" element={<SelecionarDuplas />} />
         <Route path="/partida" element={<Partida />} />
+        <Route path="/estatisticas-gerais" element={<EstatisticasGerais />} />
       </Routes>
       <PWABadge />
     </BrowserRouter>
