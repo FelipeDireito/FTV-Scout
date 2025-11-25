25f18cd0d452baff6d619fd9e5753a29be924788
FelipeDireito <fedireito92@gmail.com>
2025-11-24 23:45:01 -0300

    feat: retorna o id do atleta ao criar

--- backend/src/atletas/schemas.py
+++ backend/src/atletas/schemas.py
@@ -2,3 +2,7 @@ from pydantic import BaseModel, Field
 
 class Atleta(BaseModel):
     nome_atleta: str = Field(..., example="Atleta1")
+    atleta_id: int | None = Field(None, example=1)
+
+    class Config:
+        from_attributes = True
