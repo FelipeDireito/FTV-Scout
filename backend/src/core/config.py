aa4af157632eb49caeb46057626df4e9299602fd
FelipeDireito <fedireito92@gmail.com>
2025-11-26 16:30:53 -0300

    feat: base de dados como dados.db

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Settings:
     PROJECT_NAME: str = "FTV_SCOUT"
     DATABASE_URL: str = os.getenv(
-        "DATABASE_URL", "sqlite:///./test.db"
+        "DATABASE_URL", "sqlite:///./dados.db"
     )
     
     # SECRET_KEY: str = os.getenv
